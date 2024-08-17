3b6ab5766ca54555974d35c3a7669f4836e82840
drewrobertsmith <drew.robert.smith@gmail.com>
2024-08-17 07:50:29 -0500

    Added get_character_counts function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     book_path = "./books/frankenstein.txt"
-    text = get_book_contents(book_path)
-    words = get_number_of_words(text)
-    print(words)
+    body_text = get_book_contents(book_path)
+    words = get_number_of_words(body_text)
+    characters = get_character_counts(body_text)
+    print(characters)
+
 
 def get_book_contents(path):
     with open(path) as f:
@@ -14,5 +16,13 @@ def get_number_of_words(body_text):
     for word in words:
         number_of_words += 1
     return number_of_words
+
+def get_character_counts(body_text):
+    character_dict = {}
+    lowered_string = body_text.lower()
+    for character in lowered_string:
+        character_dict.update({character: lowered_string.count(character)})
+        print(character)
+    return character_dict
 
 main()
