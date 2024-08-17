a4bd3c900d356dbfe8026031cb4f9b2fb7c9bbc2
drewrobertsmith <drew.robert.smith@gmail.com>
2024-08-17 08:06:07 -0500

    Updated get_character_counts function to be way faster by not duping entire book string in memory, creating new dict each time, and counting entire new string, rather doing all processing on the character level as it comes

--- main.py
+++ main.py
@@ -17,12 +17,23 @@ def get_number_of_words(body_text):
         number_of_words += 1
     return number_of_words
 
-def get_character_counts(body_text):
-    character_dict = {}
-    lowered_string = body_text.lower()
-    for character in lowered_string:
-        character_dict.update({character: lowered_string.count(character)})
-        print(character)
-    return character_dict
+# MUCH SLOWER
+# Issues: creating new full copy of the text in memory by lowercasing entire string, .update() creates a new dict each time, .count() scans entire string
+# def get_character_counts(body_text):
+#     character_dict = {}
+#     lowered_string = body_text.lower()
+#     for character in lowered_string:
+#         character_dict.update({character: lowered_string.count(character)})
+#         print(character)
+#     return character_dict
 
+def get_character_counts(body_text):
+    char = {}
+    for c in body_text:
+        lower = c.lower()
+        if lower in char:
+            char[lower] += 1
+        else:
+            char[lower] = 1
+    return char
 main()
